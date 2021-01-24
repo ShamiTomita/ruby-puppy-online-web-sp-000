cf0e13702d5cbbcf088611d61dfd6858ae88ccf3
Shami Tomita <shamitomita@gmail.com>
2021-01-24 19:36:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,14 @@ class Dog
   
   def initialize(name)
     @name = name 
-    @@all << self 
+    save 
   end 
   
+  def save
+    @@all << self
+  end 
+    
+  end 
   def self.all
     @@all
   end 
